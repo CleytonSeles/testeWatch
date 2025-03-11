ff8563862ee5ddd06c9981638f7d418fbcc7440f
cleytonseles <cleyttonselestj@gmail.com>
2025-03-11 10:50:28 -0300

    Configuração inicial do fronted

---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    dark: false,
+    themes: {
+      light: {
+        primary: '#1DB954',  // Spotify green
+        secondary: '#191414', // Spotify black
+        accent: '#607D8B',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      }
+    }
+  },
+  icons: {
+    iconfont: 'mdi'
+  }
+});
